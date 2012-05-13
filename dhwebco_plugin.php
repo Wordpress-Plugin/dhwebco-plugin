ce09860de71b9eab26d95eb4721b479bc8e1ca25
Devin Humbert <devin.humbert@gmail.com>
2012-05-13 00:08:50 -0700

    Added header with info

--- dhwebco_plugin.php
+++ dhwebco_plugin.php
@@ -1,6 +1,27 @@
 <?php
 
-/* Base class for plugins */
+/**
+ * A base class for WordPress plugins
+ * 
+ * @version 0.1
+ * @author Devin Humbert <devin.humbert@gmail.com>
+ * @link http://dhwebco.com
+ * @copyright 2011 Devin Humbert
+ * @license  GPLv3
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 if (!class_exists('dhwebco_plugin')) {
 	abstract class dhwebco_plugin {
